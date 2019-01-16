f03f9de6d1eb1be8a1954b296c2bc0451a4c32d8
jhuang292 <jhuang292@wisc.edu>
2019-01-15 19:49:45 -0600

    combine two tables with left join

---
+++ 175_combine_two_tables.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT tb1.FirstName FirstName, tb1.LastName as LastName, tb2.City as City, tb2.State as State
+FROM Person as tb1
+LEFT JOIN Address as tb2
+ON tb1.PersonId = tb2.PersonId;
