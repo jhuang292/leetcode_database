2dd063b60c27286dd84795fd8f426fb4e832a624
jhuang292 <jhuang292@wisc.edu>
2019-01-16 01:43:00 -0600

    department top three salaries

---
+++ 185_Department_Top_Three_Salaries.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT tb2.Name AS Department, tb1.Name AS Employee, tb1.Salary AS Salary
+FROM Employee AS tb1
+INNER JOIN Department AS tb2
+ON tb1.DepartmentId = tb2.Id
+WHERE (SELECT count(DISTINCT tb3.Salary)
+       FROM Employee AS tb3
+       WHERE tb3.DepartmentId = tb1.DepartmentId AND tb3.Salary > tb1.Salary) < 3;
